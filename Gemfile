275e36a5aa46b98c82a5b80af55d0b02f5659a59
Hau Nguyen <hau.nguyentrinh@asnet.com.vn>
2015-02-25 14:30:37 +0700

    change production

--- Gemfile
+++ Gemfile
@@ -56,7 +56,7 @@ group :development do
 	# # rails specific capistrano funcitons
 	# gem 'capistrano-rails', '~> 1.1.0'
 	# # integrate bundler with capistrano
-	# gem 'capistrano-bundler'
+	gem 'capistrano-bundler'
 	# # if you are using RBENV
 	# gem 'capistrano-rbenv', "~> 2.0" 
 	# gem 'capistrano-rvm'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,72 +1,72 @@
 # config valid only for Capistrano 3.1
-# lock '3.1.0'
+lock '3.1.0'
 
-# set :application, 'rails_demo'
-# set :repo_url, 'git@github.com:haunguyentrinh/rails_demo.git'
-# set :full_app_name, "rails_demo_production"
+set :application, 'rails_demo'
+set :repo_url, 'git@github.com:haunguyentrinh/rails_demo.git'
+set :full_app_name, "rails_demo_production"
 
-# # Default branch is :master
-# # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# # Default deploy_to directory is /var/www/my_app
-# # set :deploy_to, '/var/www/my_app'
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
 
-# # Default value for :scm is :git
-# set :scm, :git
+# Default value for :scm is :git
+set :scm, :git
 
-# # setup rvm.
-# # set :rbenv_type, :system # or :system, depends on your rbenv setup
-# # # set :rbenv_ruby_version, "2.0.0-p247"
+# setup rvm.
 # set :rbenv_type, :system # or :system, depends on your rbenv setup
-# set :rbenv_ruby, '1.9.3-p194'
-# set :rbenv_custom_path, '/opt/rbenv'
-# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-# set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-# set :rbenv_roles, :all # default value
-# set :bundle_roles, :all  
+# # set :rbenv_ruby_version, "2.0.0-p247"
+set :rbenv_type, :system # or :system, depends on your rbenv setup
+set :rbenv_ruby, '1.9.3-p194'
+set :rbenv_custom_path, '/opt/rbenv'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+set :bundle_roles, :all  
 
-# # how many old releases do we want to keep
-# set :keep_releases, 5
+# how many old releases do we want to keep
+set :keep_releases, 5
 
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 
-# # dirs we want symlinking to shared
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# dirs we want symlinking to shared
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set(:config_files, %w(
-#   nginx.conf
-#   database.yml
-#   log_rotation
-#   monit
-#   unicorn.rb
-#   unicorn_init.sh
-# ))
+set(:config_files, %w(
+  nginx.conf
+  database.yml
+  log_rotation
+  monit
+  unicorn.rb
+  unicorn_init.sh
+))
 
-# set(:executable_config_files, %w(
-#   unicorn_init.sh
-# ))
+set(:executable_config_files, %w(
+  unicorn_init.sh
+))
 
-# # files which need to be symlinked to other parts of the
-# # filesystem. For example nginx virtualhosts, log rotation
-# # init scripts etc.
-# set(:symlinks, [
-#   {
-#     source: "nginx.conf",
-#     link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
-#   },
-#   {
-#     source: "unicorn_init.sh",
-#     link: "/etc/init.d/unicorn_#{fetch(:full_app_name)}"
-#   },
-#   {
-#     source: "log_rotation",
-#    link: "/etc/logrotate.d/#{fetch(:full_app_name)}"
-#   },
-#   {
-#     source: "monit",
-#     link: "/etc/monit/conf.d/#{fetch(:full_app_name)}.conf"
-#   }
-# ])
+# files which need to be symlinked to other parts of the
+# filesystem. For example nginx virtualhosts, log rotation
+# init scripts etc.
+set(:symlinks, [
+  {
+    source: "nginx.conf",
+    link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
+  },
+  {
+    source: "unicorn_init.sh",
+    link: "/etc/init.d/unicorn_#{fetch(:full_app_name)}"
+  },
+  {
+    source: "log_rotation",
+   link: "/etc/logrotate.d/#{fetch(:full_app_name)}"
+  },
+  {
+    source: "monit",
+    link: "/etc/monit/conf.d/#{fetch(:full_app_name)}.conf"
+  }
+])
 
 # # in case you want to set ruby version from the file:
 # # set :rbenv_ruby, File.read('.ruby-version').strip
@@ -100,84 +100,28 @@
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-# namespace :deploy do
-#   before 'deploy:setup_config', 'nginx:remove_default_vhost'
-#   after 'deploy:setup_config', 'nginx:reload'
-#   after 'deploy:setup_config', 'monit:restart'
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       # execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
-
-#   after :publishing, :restart
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-# end
-
-
-require "bundler/capistrano"
-require "rvm/capistrano"
-
-server 'uvo1sutvjqb93o7q0si.vm.cld.sr', user: 'sysadmin', password: 'Ip2WHdV9W4', roles: %w{web app db}, primary: true
-
-set :application, "projectname"
-set :user, "username"
-set :port, 22
-set :deploy_to, "/home/#{user}/apps/#{application}"
-set :deploy_via, :remote_cache
-set :use_sudo, false
-
-set :scm, "git"
-set :repo_url, 'git@github.com:haunguyentrinh/rails_demo.git'
-set :branch, "master"
-
-
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
-
 namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+  before 'deploy:setup_config', 'nginx:remove_default_vhost'
+  after 'deploy:setup_config', 'nginx:reload'
+  # after 'deploy:setup_config', 'monit:restart'
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    puts "Now edit the config files in #{shared_path}."
-  end
-  after "deploy:setup", "deploy:setup_config"
+  after :publishing, :restart
 
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
-
-  desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
-      exit
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
     end
   end
-  before "deploy", "deploy:check_revision"
+
 end
